b517aa258998cc5e5b70117a1fa23532d1c132dc
Vivek Rawat <vivek.rawat888@gmail.com>
2020-11-26 23:54:45 +0530

    added comments and changed wordCount>mapChar

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 	"golang.org/x/net/html"
 )
 
+// flag variables
 var url *string
 var articleClass *string
 var textTag *string
+var ifStats bool
 
 // write a better error handler
 func errHandle(err error) {
@@ -25,11 +27,10 @@ func errHandle(err error) {
 }
 
 // get the data from the URL
-func getData(mainString *string) {
+func scrapeData(finalText *string) {
 	// get date from the URL
 	res, err := http.Get(*url)
 	errHandle(err)
-	//	fmt.Printf("status code: %s\n", res.Status)
 	if res.StatusCode != 200 {
 		errHandle(errors.New("error code from the server"))
 	}
@@ -39,20 +40,30 @@ func getData(mainString *string) {
 	// read the document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	errHandle(err)
-	doc.Find(*articleClass).Each(func(i int, s *goquery.Selection) {
-		// sss store temp value
-		var sss string
 
-		s.Find(*textTag).Each(func(j int, ss *goquery.Selection) {
-			c := ss.Children()
+	// query the article element
+	doc.Find(*articleClass).Each(func(i int, a *goquery.Selection) {
+		// temp store text
+		var text string
+
+		// query the text element
+		a.Find(*textTag).Each(func(j int, t *goquery.Selection) {
+			// get the number of children
+			c := t.Children()
 			n := len(c.Nodes)
+
+			// ignore the elements that have children
 			if n == 0 {
-				sss = strings.TrimSpace(ss.Text())
-				if sss != "" {
+				// get the text form the element
+				text = strings.TrimSpace(t.Text())
+				// add the text to the final text
+				// if the text is not blank
+				if text != "" {
+					// add extra space if the no the 1st element
 					if j != 0 {
-						*mainString += (" " + sss)
+						*finalText += (" " + text)
 					} else {
-						*mainString += sss
+						*finalText += text
 					}
 				}
 			}
@@ -62,53 +73,71 @@ func getData(mainString *string) {
 	})
 }
 
+// count the no of HTML children
 func numberOfChild(n *html.Node) int {
 	if n == nil {
 		return -1
 	}
 	count := 0
+
+	// c is the current child
+	// loop until c is null and iterate the next sibling
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		count++
 	}
+
 	return count
 }
 
-func wordCount(s *string) (int, map[string]int) {
-	words := strings.Fields(*s)
+// map the character in the given text
+func mapChar(s *string) map[string]int {
 	m := make(map[string]int)
 	for i := range *s {
 		m[string((*s)[i])]++
 	}
-	return len(words), m
+	return m
 }
 
 // display stats
 func stats(s *string) {
 	// get the no. of characters
 	c := len(*s)
-	w, m := wordCount(s)
+	m := mapChar(s)
 
-	fmt.Printf("no. of \ncharacters: %d\nwords: %d\nmap: %v", c, w, m)
+	// divide the string to words
+	words := strings.Fields(*s)
+
+	fmt.Printf("no. of \ncharacters: %d\nwords: %d\nmap: %v", c, len(words), m)
+
+	// display in a different format
+	// sort the data
+	// print the map of the character
 	for i := range m {
 		fmt.Println(i, ":", m[i])
 	}
 }
 
 func main() {
-	var statsB bool
+	// get the flag data
 	url = flag.String("u", "", "URL for the web article")
 	articleClass = flag.String("a", "", "URL for the web article")
 	textTag = flag.String("t", "p", "tag of text")
-	flag.BoolVar(&statsB, "s", false, "tag of text")
+	flag.BoolVar(&ifStats, "s", false, "show stats of the text")
 
 	flag.Parse()
 
-	var s string
+	// check if the required flags are not nil
 
-	getData(&s)
-	fmt.Println(s)
+	// store the text
+	var text string
 
-	if statsB {
-		stats(&s)
+	// scrape data from the URL
+	scrapeData(&text)
+
+	fmt.Println(text)
+
+	// display stats if the flag is true
+	if ifStats {
+		stats(&text)
 	}
 }
